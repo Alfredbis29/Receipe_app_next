b55df8bf5001987e7f3c2806f5757e4aec8db02a
Alfredbis29 <kalumunabisimwa5@gmail.com>
2023-11-07 12:11:11 +0200

    style the category page

--- receipe_app_next/app/categories/[category]/page.tsx
+++ receipe_app_next/app/categories/[category]/page.tsx
@@ -8,11 +8,12 @@ const CategoryDetails = async (props: {params: {category: string}}) => {
     const { category } = params
     const res = await  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`)
     const data = await res.json();
-    console.log(data) 
+    console.log(data)
   return (
-    <div>
-        <h1>Category Details</h1>
-        <p>{category.description}</p>
+    <div className='bg-[#ffa60027] grid gap-4 border rouded w-full top-4 left-4 m'>
+        <h1 className='text-center gap-3 text-2xl mt-4'>Category Details</h1>
+        {/* <p>{category.description}</p> */}
+        <div className=''>
         {data?.meals?.map(
             (recipe : any) => < Recipe
             key={recipe.idMeal}
@@ -23,6 +24,7 @@ const CategoryDetails = async (props: {params: {category: string}}) => {
             }}
             />
         )}
+        </div>
       </div>
   )
 }
